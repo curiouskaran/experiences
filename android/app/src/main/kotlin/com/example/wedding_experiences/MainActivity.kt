759a97b2910e4abb8d530e229bbdf9f1d668873b
Karan Sharma <01karan95@gmail.com>
2019-10-22 07:17:09 +0530

    initial commit with project structure

---
+++ android/app/src/main/kotlin/com/example/wedding_experiences/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wedding_experiences
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
